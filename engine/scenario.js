569dc93200d8a3f788e61a22057a16d52b4ae6aa
Enrique Pineda <enrique.pineda@gmail.com>
2020-10-24 19:50:24 +0000

    base scenario class does nothing at all

--- engine/scenario.js
+++ engine/scenario.js
@@ -4,11 +4,10 @@
 
 const Concert = require('concert')
 
-
-function Scenario(title, description, play = () => { throw 'not implemented' }) {
+function Scenario(title = '', description = '', play = () => { throw 'not implemented' }) {
     this.prototype.title = title
     this.prototype.description = description
-    this.play = play
+    this.prototype.play = play
     this.maxTurns = 0
 }
 
