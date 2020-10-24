fd84acfa310e5d8207e682a52d78f67057bafb2b
Enrique Pineda <enrique.pineda@gmail.com>
2020-10-24 20:11:53 +0000

    fix incorrect prototypical member initialization

--- engine/scenario.js
+++ engine/scenario.js
@@ -5,8 +5,8 @@
 const Concert = require('concert')
 
 function Scenario(title = '', description = '', play = () => { throw 'not implemented' }) {
-    this.prototype.title = title
-    this.prototype.description = description
-    this.prototype.play = play
+    Scenario.prototype.title = title
+    Scenario.prototype.description = description
+    Scenario.prototype.play = play
     this.maxTurns = 0
 }
