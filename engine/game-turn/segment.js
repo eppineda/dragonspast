0f60b54fdc04f34e2b352ed02acc282794553117
Enrique Pineda <enrique.pineda@gmail.com>
2020-10-25 04:01:54 +0000

    test game-turn segment descriptions for player-turn and bookkeeping

--- engine/game-turn/segment.js
+++ engine/game-turn/segment.js
@@ -12,6 +12,8 @@ const names = {
 const descriptions = {
     DIPLOMACY: 'Each player secretly allocates the diplomacy points he receives this turn.',
     RANDOM_EVENTS : 'Roll the dice twice and find the corresponding event in the Random Events Table (on the accompanying pull-outs). The event then takes place accoding to the instructions for the event rolled.',
+    PLAYER_TURN: 'Each player takes his player-turn. The sequence in which the players take their player-turn depends on the scenario being played.',
+    BOOKKEEPING: 'If the game-turn being completed is the last turn of the game, play is halted, and the players assess their performance according to the victory conditions of the the scenario being played (i.e., they check to see who won). Otherwise, advance the game-turn marker on the turn track. If the red moon marker is used in that particular scenario, it is advanced.',
 }
 Segment.prototype.name = ''
 Segment.prototype.description = ''
